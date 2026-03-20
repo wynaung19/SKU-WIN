77e589152ac855726323b5a4de4a1d287511a910
Wai Yan Naung <60054637+wynaung19@users.noreply.github.com>
2026-03-20 10:24:58 +0900

    day3

--- SKU WIN/Program.cs
+++ SKU WIN/Program.cs
@@ -1,10 +1,40 @@
-﻿namespace SKU_WIN
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SKU_WIN
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Console.WriteLine("Hello C# Programming...!");
+            //Console.WriteLine(52);
+            //Console.WriteLine(52 + 233);
+            //Console.WriteLine(5 + 3 * 2);
+            //Console.WriteLine(10 % 5);
+            //Console.WriteLine(7 % 3);
+            //Console.WriteLine(1 + 2);
+            //Console.WriteLine(1 - 2);
+            //Console.WriteLine(1 * 2);
+            //Console.WriteLine(1 / 2);
+            //Console.WriteLine(1 % 2);
+
+            //Console.WriteLine(4 % 3);
+            //Console.WriteLine(-4 % 3);
+            //Console.WriteLine(4 % -3);
+            //Console.WriteLine(-4 % -3);
+
+            //Console.WriteLine(52.273);
+            //Console.WriteLine(0);
+            //Console.WriteLine(0.0);
+
+            Console.WriteLine(1.0 + 2.0);
+            Console.WriteLine(1.0 - 2.0);
+            Console.WriteLine(1.0 * 2.0);
+            Console.WriteLine(1.0 / 2.0);
         }
     }
 }
