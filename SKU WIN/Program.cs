786ed30f04416813d8217786a092af6a35b61f36
Wai Yan Naung <60054637+wynaung19@users.noreply.github.com>
2026-03-27 10:38:23 +0900

    Day 4

--- SKU WIN/Program.cs
+++ SKU WIN/Program.cs
@@ -1,107 +1,109 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace SKU_WIN
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            //Console.WriteLine("Hello C# Programming...!");
-            //Console.WriteLine(52);
-            //Console.WriteLine(52 + 233);
-            //Console.WriteLine(5 + 3 * 2);
-            //Console.WriteLine(10 % 5);
-            //Console.WriteLine(7 % 3);
-            //Console.WriteLine(1 + 2);
-            //Console.WriteLine(1 - 2);
-            //Console.WriteLine(1 * 2);
-            //Console.WriteLine(1 / 2);
-            //Console.WriteLine(1 % 2);
+//namespace SKU_WIN
+//{
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {
 
-            //Console.WriteLine(4 % 3);
-            //Console.WriteLine(-4 % 3);
-            //Console.WriteLine(4 % -3);
-            //Console.WriteLine(-4 % -3);
 
-            //Console.WriteLine(52.273);
-            //Console.WriteLine(0);
-            //Console.WriteLine(0.0);
+//            //Console.WriteLine("Hello C# Programming...!");
+//            //Console.WriteLine(52);
+//            //Console.WriteLine(52 + 233);
+//            //Console.WriteLine(5 + 3 * 2);
+//            //Console.WriteLine(10 % 5);
+//            //Console.WriteLine(7 % 3);
+//            //Console.WriteLine(1 + 2);
+//            //Console.WriteLine(1 - 2);
+//            //Console.WriteLine(1 * 2);
+//            //Console.WriteLine(1 / 2);
+//            //Console.WriteLine(1 % 2);
 
-            //Console.WriteLine(1.0 + 2.0);
-            //Console.WriteLine(1.0 - 2.0);
-            //Console.WriteLine(1.0 * 2.0);
-            //Console.WriteLine(1.0 / 2.0);
+//            //Console.WriteLine(4 % 3);
+//            //Console.WriteLine(-4 % 3);
+//            //Console.WriteLine(4 % -3);
+//            //Console.WriteLine(-4 % -3);
 
-            //Console.WriteLine('A');
-            //Console.WriteLine('가');
-            //Console.WriteLine('나');
+//            //Console.WriteLine(52.273);
+//            //Console.WriteLine(0);
+//            //Console.WriteLine(0.0);
 
-            //Console.WriteLine("안녕하세요");
-            //Console.WriteLine("Text\tHere");
-            //Console.WriteLine("Text\nHere");
-            //Console.WriteLine("\"\"\"");
+//            //Console.WriteLine(1.0 + 2.0);
+//            //Console.WriteLine(1.0 - 2.0);
+//            //Console.WriteLine(1.0 * 2.0);
+//            //Console.WriteLine(1.0 / 2.0);
 
-            //Console.WriteLine("가나다" + "라마" + "바사아" + "자차카타" + "파하");
+//            //Console.WriteLine('A');
+//            //Console.WriteLine('가');
+//            //Console.WriteLine('나');
 
-            //Console.WriteLine("안녕하세요"[0]);
-            //Console.WriteLine("안녕하세요"[1]);
-            //Console.WriteLine("안녕하세요"[3]);
+//            //Console.WriteLine("안녕하세요");
+//            //Console.WriteLine("Text\tHere");
+//            //Console.WriteLine("Text\nHere");
+//            //Console.WriteLine("\"\"\"");
 
-            //Console.WriteLine("안녕하세요"[100]);
+//            //Console.WriteLine("가나다" + "라마" + "바사아" + "자차카타" + "파하");
 
-            //Console.WriteLine(true);
-            //Console.WriteLine(false);
+//            //Console.WriteLine("안녕하세요"[0]);
+//            //Console.WriteLine("안녕하세요"[1]);
+//            //Console.WriteLine("안녕하세요"[3]);
 
-            //Console.WriteLine(52 < 273);
-            //Console.WriteLine(52 > 273);
+//            //Console.WriteLine("안녕하세요"[100]);
 
-            //Console.WriteLine(!true);
-            //Console.WriteLine(!false);
-            //Console.WriteLine(!(52 < 273));
-            //Console.WriteLine(!(52 > 273));
+//            //Console.WriteLine(true);
+//            //Console.WriteLine(false);
 
-            //Console.WriteLine(DateTime.Now.Hour < 3 || 8 < DateTime.Now.Hour);
-            //Console.WriteLine(3 < DateTime.Now.Hour && DateTime.Now.Hour < 8);
+//            //Console.WriteLine(52 < 273);
+//            //Console.WriteLine(52 > 273);
 
-            //int a = 272;
-            //int b = 52;
+//            //Console.WriteLine(!true);
+//            //Console.WriteLine(!false);
+//            //Console.WriteLine(!(52 < 273));
+//            //Console.WriteLine(!(52 > 273));
 
-            //Console.WriteLine(a + b);
-            //Console.WriteLine(a - b);
-            //Console.WriteLine(a * b);
-            //Console.WriteLine(a / b);
-            //Console.WriteLine(a % b);
+//            //Console.WriteLine(DateTime.Now.Hour < 3 || 8 < DateTime.Now.Hour);
+//            //Console.WriteLine(3 < DateTime.Now.Hour && DateTime.Now.Hour < 8);
 
-            //int a = 2000000000;
-            //int b = 1000000000;
-            //Console.WriteLine(a + b);
+//            //int a = 272;
+//            //int b = 52;
 
-            //double a = 52.234;
-            //double b = 123.34;
+//            //Console.WriteLine(a + b);
+//            //Console.WriteLine(a - b);
+//            //Console.WriteLine(a * b);
+//            //Console.WriteLine(a / b);
+//            //Console.WriteLine(a % b);
 
-            //Console.WriteLine(a + b);
-            //Console.WriteLine(a - b);
-            //Console.WriteLine(a * b);
-            //Console.WriteLine(a / b);
+//            //int a = 2000000000;
+//            //int b = 1000000000;
+//            //Console.WriteLine(a + b);
 
-            //char a = 'a';
-            //Console.WriteLine(a);
+//            //double a = 52.234;
+//            //double b = 123.34;
 
-            //string msg = "Hello";
-            //Console.WriteLine(msg + "!");
-            //Console.WriteLine(msg[0]);
-            //Console.WriteLine(msg[1]);
-            //Console.WriteLine(msg[2]);
+//            //Console.WriteLine(a + b);
+//            //Console.WriteLine(a - b);
+//            //Console.WriteLine(a * b);
+//            //Console.WriteLine(a / b);
 
-            bool one = 10 < 0;
-            bool other = 20 > 100;
+//            //char a = 'a';
+//            //Console.WriteLine(a);
 
-            Console.WriteLine(one);
-            Console.WriteLine(other);
-        }
-    }
-}
+//            //string msg = "Hello";
+//            //Console.WriteLine(msg + "!");
+//            //Console.WriteLine(msg[0]);
+//            //Console.WriteLine(msg[1]);
+//            //Console.WriteLine(msg[2]);
+
+//            //bool one = 10 < 0;
+//            //bool other = 20 > 100;
+
+//            //Console.WriteLine(one);
+//            //Console.WriteLine(other);
+//        }
+//    }
+//}
---
+++ SKU WIN/day4.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SKU_WIN
+{
+    internal class day4
+    {
+        static void Main(string[] args)
+        {
+            //int output = 0;
+            //output += 52;
+            //output += 273;
+            //output += 103;
+            //Console.WriteLine(output);
+
+            //string output = "Hello ";
+            //output += "World";
+            //output += "!";
+            //Console.WriteLine(output);
+
+            //int num = 10;
+            //num++;
+            //Console.WriteLine(num);
+            //num--;
+            //Console.WriteLine(num);
+
+            //int num = 10;
+            //Console.WriteLine(num);
+            //Console.WriteLine(num++);
+            //Console.WriteLine(num--);
+            //Console.WriteLine(num);
+
+            //int num = 10;
+            //Console.WriteLine(num);
+            //Console.WriteLine(++num);
+            //Console.WriteLine(--num);
+            //Console.WriteLine(num);
+
+            //int num = 10;
+            //Console.WriteLine(num);
+            //num++;
+            //num++;
+            //Console.WriteLine(num);
+            //Console.WriteLine(num);
+            //num--;
+            //num--;
+            //Console.WriteLine(num);
+
+            //int num = 10;
+            //Console.WriteLine(num++);
+            //Console.WriteLine(++num);
+            //Console.WriteLine(num--);
+            //Console.WriteLine(--num);
+
+            //int _int = 273;
+            //long _long = 2343243243243242;
+            //float _float = 23.11F;
+            //double _double = 23.343423;
+            //char _char = 'A';
+            //string _string = "WaiYanNaung";
+
+            //Console.WriteLine(_int.GetType());
+            //Console.WriteLine(_long.GetType());
+            //Console.WriteLine(_float.GetType());
+            //Console.WriteLine(_double.GetType());
+            //Console.WriteLine(_char.GetType());
+            //Console.WriteLine(_string.GetType());
+
+            //Console.WriteLine((233).GetType());
+            //Console.WriteLine((23432423432423L).GetType());
+            //Console.WriteLine((52.232F).GetType());
+            //Console.WriteLine((53.343).GetType());
+            //Console.WriteLine(('A').GetType());
+            //Console.WriteLine(("WYN").GetType());
+
+            //string inp = Console.ReadLine();
+            //Console.WriteLine("input : "+inp);
+
+            // --- ERROR
+            //long longNum = 2147483647L + 2147483647L;
+            //int intNum = longNum;
+            //Console.WriteLine(intNum);
+
+            //long longNum = 2147483647L + 2147483647L;
+            //int intNum = (int)longNum;
+            //Console.WriteLine(intNum);
+
+            //long longNum = 52343;
+            //int intNum = (int)longNum;
+            //Console.WriteLine(intNum);
+
+            //long longNum = 2147483647L + 2147483647L;
+            //int longToInt = (int)longNum;
+            //Console.WriteLine(longToInt);
+
+            //double douNum = 52.27310332;
+            //int douToint = (int)douNum;
+            //Console.WriteLine(douToint);
+
+            //int intNumber = 2147483647;
+            //long intToLong = intNumber;
+            //Console.WriteLine(intToLong);
+
+            //double intToDouble = intNumber;
+            //Console.WriteLine(intToDouble);
+
+            //Console.WriteLine(int.Parse("52"));
+            //Console.WriteLine(long.Parse("263"));
+            //Console.WriteLine(float.Parse("52.3373"));
+            //Console.WriteLine(double.Parse("113.32"));
+            //Console.WriteLine(int.Parse("52").GetType());
+            //Console.WriteLine(long.Parse("273").GetType());
+            //Console.WriteLine(float.Parse("52.263").GetType());
+            //Console.WriteLine(double.Parse("103.32").GetType());
+
+            //Console.WriteLine((52).ToString());
+            //Console.WriteLine((52.23423).ToString());
+            //Console.WriteLine(('a').ToString());
+            //Console.WriteLine((true).ToString());
+            //Console.WriteLine((false).ToString());
+            //Console.WriteLine((52).ToString().GetType());
+            //Console.WriteLine((52.23423).ToString().GetType());
+            //Console.WriteLine(('a').ToString().GetType());
+            //Console.WriteLine((true).ToString().GetType());
+            //Console.WriteLine((false).ToString().GetType());
+
+            //double num = 52.234324;
+            //Console.WriteLine(num.ToString("0.0"));
+            //Console.WriteLine(num.ToString("0.00"));
+            //Console.WriteLine(num.ToString("0.000"));
+            //Console.WriteLine(num.ToString("0.0000"));
+
+            //Console.WriteLine(52 + 237);
+            //Console.WriteLine("52" + 237);
+            //Console.WriteLine(52 + "237");
+            //Console.WriteLine("52" + "237");
+
+            //int num = 52273;
+            //string outA = num + "";
+            //Console.WriteLine(outA);
+            //Console.WriteLine(outA.GetType());
+
+            //char cha = 'a';
+            //string outB = cha + "";
+            //Console.WriteLine(outB);
+            //Console.WriteLine(outB.GetType());
+
+            Console.WriteLine(bool.Parse("True"));
+            Console.WriteLine(bool.Parse("true"));
+            Console.WriteLine(bool.Parse("False"));
+            Console.WriteLine(bool.Parse("false"));
+
+        }
+    }
+}
