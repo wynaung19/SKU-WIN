a27d09cea842ac65adbc98dfe55b52c265bda84a
Wai Yan Naung <60054637+wynaung19@users.noreply.github.com>
2026-04-10 11:03:44 +0900

    D6 end

--- SKU WIN/day6.cs
+++ SKU WIN/day6.cs
@@ -65,10 +65,147 @@ namespace SKU_WIN
             //    Console.WriteLine(intArr[i]);
             //}
 
-            string[] arr = { "A", "B", "C", "D", "E" };
-            foreach (string item in arr)
+            //string[] arr = { "A", "B", "C", "D", "E" };
+            //foreach (string item in arr)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            //string[] arr = { "A", "B", "C", "D", "E" };
+            //foreach (var item in arr)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    for(int j = 0; j < i+1; j++)
+            //    {
+            //        Console.Write('*');
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    for (int j = 0; j < 10 - i; j++)
+            //    {
+            //        Console.Write(' ');
+            //    }
+            //    for (int j = 0; j < i + 1; j++)
+            //    {
+            //        Console.Write('*');
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            //while (true)
+            //{
+            //    Console.WriteLine("Enter Number (Even to exit):");
+            //    int input = int.Parse(Console.ReadLine());
+            //    if (input % 2 == 0)
+            //    {
+            //        break;
+            //    }
+            //}
+
+            //    for(int i = 0; i<10;i++)
+            //    {
+            //        Console.WriteLine("Out");
+            //        for(int j =0; j<10;j++)
+            //        {
+            //            Console.WriteLine("In");
+            //            goto doNotUse;
+            //        }
+            //    }
+            //doNotUse:
+            //        Console.WriteLine("Out of Loop");
+
+            //for(int i = 1; i <10; i++)
+            //{
+            //    if(i %2 == 0)
+            //    {
+            //        continue;
+            //    }
+            //    Console.WriteLine(i);
+            //}
+
+            //string input = "Wai Yan Naung";
+            ////Console.WriteLine(input.ToUpper());
+            ////Console.WriteLine(input.ToLower());
+
+            //string[] arr = input.Split(new char[] { ' ' });
+            //foreach (string item in arr)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            //string inp = " Wai Yan Naung            \n";
+            //Console.WriteLine("::" + inp.Trim() + "::");
+            //Console.Read();
+
+            //string[] arr = { "A", "B", "C", "D", "E" };
+            //Console.WriteLine(string.Join(",", arr));
+
+            //Console.WriteLine("Before");
+            //Console.SetCursorPosition(5, 5);
+            //Console.WriteLine("After");
+
+            //Console.WriteLine("Out 1");
+            //Thread.Sleep(1000);
+            //Console.WriteLine("Out 2");
+            //Thread.Sleep(1000);
+            //Console.WriteLine("Out 3");
+            //Thread.Sleep(1000);
+            //Console.WriteLine("Out 4");
+            //Thread.Sleep(1000);
+            //Console.WriteLine("Out 5");
+
+            //int x = 1;
+            //while (x < 50)
+            //{
+            //    Console.Clear();
+            //    Console.SetCursorPosition(x, 5);
+
+            //    if (x % 3 == 0)
+            //    {
+            //        Console.WriteLine("__@");
+            //    }
+            //    else if(x % 3 == 1)
+            //    {
+            //        Console.WriteLine("_^@");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("^_@");
+            //    }
+            //    x++;
+            //    Thread.Sleep(500);
+            //}
+
+            bool state = true;
+            while (state)
             {
-                Console.WriteLine(item);
+                ConsoleKeyInfo Info = Console.ReadKey();
+                switch (Info.Key)
+                {
+                    case ConsoleKey.UpArrow:
+                        Console.WriteLine("Up Arrow");
+                        break;
+                    case ConsoleKey.DownArrow:
+                        Console.WriteLine("Down Arrow");
+                        break;
+                    case ConsoleKey.LeftArrow:
+                        Console.WriteLine("Left Arrow");
+                        break;
+                    case ConsoleKey.RightArrow:
+                        Console.WriteLine("Right Arrow");
+                        break;
+                    default:
+                        Console.WriteLine("Other Key");
+                        state = false;
+                        break;
+                }
             }
         }
     }
