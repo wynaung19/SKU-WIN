8ffc29f34107ef67473dba68439d322324719265
Wai Yan Naung <60054637+wynaung19@users.noreply.github.com>
2026-04-10 10:18:07 +0900

    day6 first

---
+++ SKU WIN/day5.cs
@@ -0,0 +1,235 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Diagnostics;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace SKU_WIN
+//{
+//    internal class Day5
+//    {
+//        static void Main(string[] args)
+//        {
+//            //Console.WriteLine("Enter Number");
+//            //int input = int.Parse(Console.ReadLine());
+
+//            //if (input % 2 == 0)
+//            //{
+//            //    Console.WriteLine("Even");
+//            //}
+//            //if (input % 2 == 1)
+//            //{
+//            //    Console.WriteLine("ODD");
+//            //}
+//            //Console.WriteLine(DateTime.Now.Year);
+//            //Console.WriteLine(DateTime.Now.Month);
+//            //Console.WriteLine(DateTime.Now.Day);
+//            //Console.WriteLine(DateTime.Now.Hour);
+//            //Console.WriteLine(DateTime.Now.Minute);
+//            //Console.WriteLine(DateTime.Now.Second);
+
+//            //if(DateTime.Now.Hour < 12)
+//            //{
+//            //    Console.WriteLine("AM");
+//            //}
+//            //if (DateTime.Now.Hour >= 12)
+//            //{
+//            //    Console.WriteLine("PM");
+//            //}
+//            //Console.WriteLine("Enter Number");
+//            //int input = int.Parse(Console.ReadLine());
+
+//            //if (input % 2 == 0)
+//            //{
+//            //    Console.WriteLine("Even");
+//            //}
+//            //else
+//            //{
+//            //    Console.WriteLine("ODD");
+//            //}
+
+//            //if (DateTime.Now.Hour < 12)
+//            //{
+//            //    Console.WriteLine("AM");
+//            //}
+//            //else
+//            //{
+//            //    Console.WriteLine("PM");
+//            //}
+
+//            //if (DateTime.Now.Hour < 11)
+//            //{
+//            //    Console.WriteLine("아침 먹을 시간입니다.");
+//            //}
+//            //else
+//            //{
+//            //    if (DateTime.Now.Hour < 15)
+//            //    {
+//            //        Console.WriteLine("점심 먹을 시간입니다.");
+//            //    }
+//            //    else
+//            //    {
+//            //        Console.WriteLine("저녁 먹을 시간입니다.");
+//            //    }
+//            //}
+
+//            //if (DateTime.Now.Hour < 11)
+//            //{
+//            //    Console.WriteLine("아침 먹을 시간입니다.");
+//            //}
+//            //else if (DateTime.Now.Hour < 15)
+//            //{
+//            //    Console.WriteLine("점심 먹을 시간입니다.");
+//            //}
+//            //else
+//            //{
+//            //    Console.WriteLine("저녁 먹을 시간입니다.");
+//            //}
+
+//            //Console.WriteLine("Enter Score");
+//            //int score = int.Parse(Console.ReadLine());
+//            //Console.WriteLine("\n\n");
+//            //if(score >= 90)
+//            //{
+//            //    Console.WriteLine("A");
+//            //}
+//            //else if (score >= 80)
+//            //{
+//            //    Console.WriteLine("B");
+//            //}
+//            //else if (score >= 70)
+//            //{
+//            //    Console.WriteLine("C");
+//            //}
+//            //else if (score >= 60)
+//            //{
+//            //    Console.WriteLine("D");
+//            //}
+//            //else
+//            //{
+//            //    Console.WriteLine("E");
+//            //}
+
+//            //double score = 3.6;
+
+//            //if(score == 4.5)
+//            //{
+//            //    Console.WriteLine("Perfect");
+//            //}
+//            //else if (score <= 4.2 && score < 4.5)
+//            //{
+//            //    Console.WriteLine("Good Job!");
+//            //}
+//            //else if (score <= 3.8 && score < 4.2)
+//            //{
+//            //    Console.WriteLine("Great");
+//            //}
+//            //else if (score <= 2.5 && score < 3.8)
+//            //{
+//            //    Console.WriteLine("Average");
+//            //}
+//            //else if (score < 0 && score < 2.5)
+//            //{
+//            //    Console.WriteLine("I know 5 stupid people and you are 4 of them.");
+//            //}
+
+//            //Console.WriteLine("Enter Number");
+//            //int input = int.Parse(Console.ReadLine());
+
+//            //switch(input % 2)
+//            //{
+//            //    case 0:
+//            //        Console.WriteLine("Even");
+//            //        break;
+//            //    case 1:
+//            //        Console.WriteLine("Odd");
+//            //        break;
+//            //}
+
+//            //Console.WriteLine("Enter Month");
+//            //int input = int.Parse(Console.ReadLine());
+
+//            //switch (input)
+//            //{
+//            //    case 12:
+//            //    case 1:
+//            //    case 2:
+//            //        Console.WriteLine("겨울입니다.");
+//            //        break;
+//            //    case 3:
+//            //    case 4:
+//            //    case 5:
+//            //        Console.WriteLine("봄입니다.");
+//            //        break;
+//            //    case 6:
+//            //    case 7:
+//            //    case 8:
+//            //        Console.WriteLine("여름입니다.");
+//            //        break;
+//            //    case 9:
+//            //    case 10:
+//            //    case 11:
+//            //        Console.WriteLine("가을입니다.");
+//            //        break;
+//            //    default:
+//            //        Console.WriteLine("대체 어떤 행성에 살고 계신가요?");
+//            //        break;
+//            //}
+
+//            //Console.WriteLine("Enter Number");
+//            //int input = int.Parse(Console.ReadLine());
+
+//            //Console.WriteLine(input % 2 == 0 ? "Even" : "ODD");
+
+//            Console.WriteLine("Enter sccore");
+//            int score = 0; score =  int.Parse(Console.ReadLine());
+
+//            //if (score > 60)
+//            //{
+//            //    Console.WriteLine("통과");
+//            //}
+//            //else
+//            //{
+//            //    Console.WriteLine("재수강");
+//            //}
+
+//            //double score = 0;
+//            //score = double.Parse(Console.ReadLine());
+
+//            //if (score >= 90)
+//            //{
+//            //    Console.WriteLine("A");
+//            //}
+//            //else if (score >= 80)
+//            //{
+//            //    Console.WriteLine("B");
+//            //}
+//            //else if (score >= 70)
+//            //{
+//            //    Console.WriteLine("C");
+//            //}
+//            //else
+//            //{
+//            //    Console.WriteLine("F");
+//            //}
+
+//            //switch (score/10)
+//            //{
+//            //    case 10:
+//            //    case 9:
+//            //        Console.WriteLine("A");
+//            //        break;
+//            //    case 8:
+//            //        Console.WriteLine("B");
+//            //        break;
+//            //    case 7:
+//            //        Console.WriteLine("C");
+//            //        break;
+//            //    default:
+//            //        Console.WriteLine("F");
+//            //        break;
+//            //}
+//        }
+//    }
+//}
---
+++ SKU WIN/day6.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SKU_WIN
+{
+    internal class Day6
+    {
+        static void Main(string[] args)
+        {
+            //int[] intArr = { 1, 2, 3, 4, 5 };
+            //intArr[0] = 10;
+            //Console.WriteLine(intArr[0]);
+            //Console.WriteLine(intArr[1]);
+            //Console.WriteLine(intArr[2]);
+            //Console.WriteLine(intArr[3]);
+            //Console.WriteLine(intArr[4]);
+
+            //int[] intArr = new int[100];
+            //Console.WriteLine(intArr[0]);
+            //Console.WriteLine(intArr[1]);
+
+            //int i = 0;
+            //int[] intArr = { 23, 52, 273, 103, 34 };
+
+            //while (i < intArr.Length)
+            //{
+            //    Console.WriteLine(i + " > " + intArr[i]);
+            //    i++;
+            //}
+
+            //string input;
+            //do
+            //{
+            //    Console.WriteLine("Enter exit to exit.");
+            //    input = Console.ReadLine();
+            //}
+            //while (input != "exit");
+
+            //int output = 0;
+            //for(int i =0;i<=100;i++)
+            //{
+            //    output += i;
+            //}
+            //Console.WriteLine(output);
+
+            //int output = 1;
+            // for (int i = 1; i <= 20; i++)
+            // {
+            //     output *= i;
+            // }
+            // Console.WriteLine(output);
+
+            //for (int i = 'A'; i <= 'Z'; i++)
+            //{
+            //    Console.WriteLine((char)i);
+            //}
+
+            //int[] intArr = { 1, 2, 3, 4, 5 };
+            //for (int i = intArr.Length - 1; i >= 0; i--)
+            //{
+            //    Console.WriteLine(intArr[i]);
+            //}
+
+            string[] arr = { "A", "B", "C", "D", "E" };
+            foreach (string item in arr)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
