335bd5f1061590bc4ae04ea946f32a73b97e17c9
Wai Yan Naung <60054637+wynaung19@users.noreply.github.com>
2026-05-08 09:44:32 +0900

    5/8 1

--- SKU WIN/Day7.cs
+++ SKU WIN/Day7.cs
@@ -1,71 +1,71 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Net.Http.Headers;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Diagnostics;
+//using System.Linq;
+//using System.Net.Http.Headers;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace SKU_WIN
-{
-    internal class Day7
-    {
-        //class Product
-        //{
-        //    public string name;
-        //    public int price;
-        //}
+//namespace SKU_WIN
+//{
+//    internal class Day7
+//    {
+//        //class Product
+//        //{
+//        //    public string name;
+//        //    public int price;
+//        //}
 
-        class MyMath
-        {
-            public static double PI = 3.141592;
-        }
-        static void Main(string[] args)
-        {
-            //Product product = new Product();
-            //product.name = "Laptop";
-            //product.price = 50000;
-            //Console.WriteLine("Product Name: " + product.name);
-            //Console.WriteLine("Product Price: " + product.price);
+//        class MyMath
+//        {
+//            public static double PI = 3.141592;
+//        }
+//        static void Main(string[] args)
+//        {
+//            //Product product = new Product();
+//            //product.name = "Laptop";
+//            //product.price = 50000;
+//            //Console.WriteLine("Product Name: " + product.name);
+//            //Console.WriteLine("Product Price: " + product.price);
 
 
-            Console.WriteLine(MyMath.PI);
+//            Console.WriteLine(MyMath.PI);
 
-            //Random random = new Random();
-            //Console.WriteLine(random.Next(1, 100));
-            //Console.WriteLine(random.Next(1, 100));
-            //Console.WriteLine(random.Next(1, 100));
-            //Console.WriteLine(random.Next(1, 100));
-            //Console.WriteLine(random.Next(1, 100));
+//            //Random random = new Random();
+//            //Console.WriteLine(random.Next(1, 100));
+//            //Console.WriteLine(random.Next(1, 100));
+//            //Console.WriteLine(random.Next(1, 100));
+//            //Console.WriteLine(random.Next(1, 100));
+//            //Console.WriteLine(random.Next(1, 100));
 
-            //Console.WriteLine(random.NextDouble());
-            //Console.WriteLine(random.NextDouble());
-            //Console.WriteLine(random.NextDouble());
-            //Console.WriteLine(random.NextDouble());
+//            //Console.WriteLine(random.NextDouble());
+//            //Console.WriteLine(random.NextDouble());
+//            //Console.WriteLine(random.NextDouble());
+//            //Console.WriteLine(random.NextDouble());
 
-            //List<int> list = new List<int>();
-            //list.Add(32);
-            //list.Add(12);
-            //list.Add(45);
-            //list.Add(67);
-            //foreach (int item in list)
-            //{
-            //    Console.WriteLine("Count: "+list.Count + "\titem: "+item);
-            //}
+//            //List<int> list = new List<int>();
+//            //list.Add(32);
+//            //list.Add(12);
+//            //list.Add(45);
+//            //list.Add(67);
+//            //foreach (int item in list)
+//            //{
+//            //    Console.WriteLine("Count: "+list.Count + "\titem: "+item);
+//            //}
 
-            //List<int> list = new List<int>() { 34,456,78,56,23,58 };
-            //list.Remove(78);
-            //foreach (int item in list)
-            //{
-            //    Console.WriteLine("Count: " + list.Count + "\titem: " + item);
-            //}
+//            //List<int> list = new List<int>() { 34,456,78,56,23,58 };
+//            //list.Remove(78);
+//            //foreach (int item in list)
+//            //{
+//            //    Console.WriteLine("Count: " + list.Count + "\titem: " + item);
+//            //}
 
-            //Console.WriteLine(Math.Abs(-2722));
-            //Console.WriteLine(Math.Ceiling(3.14));
-            //Console.WriteLine(Math.Floor(3.14));
-            //Console.WriteLine(Math.Max(31, 59));
-            //Console.WriteLine(Math.Min(31, 59));
-            //Console.WriteLine(Math.Round(3.14));
-        }
-    }
-}
+//            //Console.WriteLine(Math.Abs(-2722));
+//            //Console.WriteLine(Math.Ceiling(3.14));
+//            //Console.WriteLine(Math.Floor(3.14));
+//            //Console.WriteLine(Math.Max(31, 59));
+//            //Console.WriteLine(Math.Min(31, 59));
+//            //Console.WriteLine(Math.Round(3.14));
+//        }
+//    }
+//}
---
+++ SKU WIN/Day8.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SKU_WIN
+{
+    class Day8
+    {
+        class Test
+        {
+            //public int Power(int x)
+            //{
+            //    return x * x;
+            //}
+
+            //public int Multiply(int x, int y)
+            //{
+            //    return x * y;
+            //}
+
+            //public void Print()
+            //{
+            //    Console.WriteLine("Print inside message");
+            //}
+
+            //public int Sum(int min, int max)
+            //{
+            //    int output = 0;
+            //    for(int i = min; i <= max; i++)
+            //    {
+            //        output += i;
+            //    }
+            //    return output;
+            //}
+
+            //public int Multiply(int min, int max)
+            //{
+            //    int output = 1;
+            //    for (int i = min; i <= max; i++)
+            //    {
+            //        output *= i;
+            //    }
+            //    return output;
+            //}
+        }
+
+        class MyMath
+        {
+            public static int Abs(int x)
+            {
+                if (x < 0)
+                    return -x;
+                else
+                    return x;
+            }
+        }
+        static void Main(string[] args)
+        {
+            //Test test = new Test();
+            //Console.WriteLine(test.Multiply(1,10));
+            Console.WriteLine(MyMath.Abs(423));
+            Console.WriteLine(MyMath.Abs(-23));
+        }
+    }
+}
