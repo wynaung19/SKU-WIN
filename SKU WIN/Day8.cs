1b223b0d20eb8d0c2c73be985ac1e84bee12c970
Wai Yan Naung <60054637+wynaung19@users.noreply.github.com>
2026-05-22 11:03:27 +0900

    5-22

--- SKU WIN/Day8.cs
+++ SKU WIN/Day8.cs
@@ -1,120 +1,120 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Net.Http.Headers;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Diagnostics;
+//using System.Linq;
+//using System.Net.Http.Headers;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace SKU_WIN
-{
-    class Day8
-    {
-        //class Test
-        //{
-        //    //public int Power(int x)
-        //    //{
-        //    //    return x * x;
-        //    //}
+//namespace SKU_WIN
+//{
+//    class Day8
+//    {
+//        //class Test
+//        //{
+//        //    //public int Power(int x)
+//        //    //{
+//        //    //    return x * x;
+//        //    //}
 
-        //    //public int Multiply(int x, int y)
-        //    //{
-        //    //    return x * y;
-        //    //}
+//        //    //public int Multiply(int x, int y)
+//        //    //{
+//        //    //    return x * y;
+//        //    //}
 
-        //    //public void Print()
-        //    //{
-        //    //    Console.WriteLine("Print inside message");
-        //    //}
+//        //    //public void Print()
+//        //    //{
+//        //    //    Console.WriteLine("Print inside message");
+//        //    //}
 
-        //    //public int Sum(int min, int max)
-        //    //{
-        //    //    int output = 0;
-        //    //    for(int i = min; i <= max; i++)
-        //    //    {
-        //    //        output += i;
-        //    //    }
-        //    //    return output;
-        //    //}
+//        //    //public int Sum(int min, int max)
+//        //    //{
+//        //    //    int output = 0;
+//        //    //    for(int i = min; i <= max; i++)
+//        //    //    {
+//        //    //        output += i;
+//        //    //    }
+//        //    //    return output;
+//        //    //}
 
-        //    //public int Multiply(int min, int max)
-        //    //{
-        //    //    int output = 1;
-        //    //    for (int i = min; i <= max; i++)
-        //    //    {
-        //    //        output *= i;
-        //    //    }
-        //    //    return output;
-        //    //}
-        //}
+//        //    //public int Multiply(int min, int max)
+//        //    //{
+//        //    //    int output = 1;
+//        //    //    for (int i = min; i <= max; i++)
+//        //    //    {
+//        //    //        output *= i;
+//        //    //    }
+//        //    //    return output;
+//        //    //}
+//        //}
 
-        class MyMath
-        {
-            public static int Abs(int x)
-            {
-                if (x < 0)
-                    return -x;
-                else
-                    return x;
-            }
+//        class MyMath
+//        {
+//            public static int Abs(int x)
+//            {
+//                if (x < 0)
+//                    return -x;
+//                else
+//                    return x;
+//            }
 
-            public static double Abs(double x)
-            {
-                if (x < 0)
-                    return -x;
-                else
-                    return x;
-            }
+//            public static double Abs(double x)
+//            {
+//                if (x < 0)
+//                    return -x;
+//                else
+//                    return x;
+//            }
 
-            public static long Abs(long x)
-            {
-                if (x < 0)
-                    return -x;
-                else
-                    return x;
-            }
-        }
+//            public static long Abs(long x)
+//            {
+//                if (x < 0)
+//                    return -x;
+//                else
+//                    return x;
+//            }
+//        }
 
-        class Fibonacci
-        {
-            public static int Get(int n)
-            {
-                if (n == 0)
-                    return 0;
-                else if (n == 1)
-                    return 1;
-                else
-                    return Get(n - 1) + Get(n - 2);
-            }
-        }
+//        class Fibonacci
+//        {
+//            public static int Get(int n)
+//            {
+//                if (n == 0)
+//                    return 0;
+//                else if (n == 1)
+//                    return 1;
+//                else
+//                    return Get(n - 1) + Get(n - 2);
+//            }
+//        }
 
-        static int Test(int x)
-        {
-            return 10;
-        }
-        static int Test(long x)
-        {
-            return 20;
-        }
-        static int Test(float x)
-        {
-            return 30;
-        }
-        static int Test(double x) {
-            return 40;
-        }
-        static void Main(string[] args)
-        {
-            //Fibonacci fibo = new Fibonacci();
-            //Console.WriteLine(Fibonacci.Get(1));
-            //Console.WriteLine(Fibonacci.Get(2));
-            //Console.WriteLine(Fibonacci.Get(3));
-            //Console.WriteLine(Fibonacci.Get(4));
-            //Console.WriteLine(Fibonacci.Get(5));
+//        static int Test(int x)
+//        {
+//            return 10;
+//        }
+//        static int Test(long x)
+//        {
+//            return 20;
+//        }
+//        static int Test(float x)
+//        {
+//            return 30;
+//        }
+//        static int Test(double x) {
+//            return 40;
+//        }
+//        static void Main(string[] args)
+//        {
+//            //Fibonacci fibo = new Fibonacci();
+//            //Console.WriteLine(Fibonacci.Get(1));
+//            //Console.WriteLine(Fibonacci.Get(2));
+//            //Console.WriteLine(Fibonacci.Get(3));
+//            //Console.WriteLine(Fibonacci.Get(4));
+//            //Console.WriteLine(Fibonacci.Get(5));
 
-            Console.WriteLine(Test(56363));
-            Console.WriteLine(Test(34.345));
-            Console.WriteLine(Test(563634623452345));
-        }
-    }
-}
+//            Console.WriteLine(Test(56363));
+//            Console.WriteLine(Test(34.345));
+//            Console.WriteLine(Test(563634623452345));
+//        }
+//    }
+//}
---
+++ SKU WIN/Day9.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SKU_WIN
+{
+    class Day9
+    {
+        //class Product
+        //{
+        //    public static int count = 0;
+        //    public int id;
+        //    public string name;
+        //    public int price;
+
+        //    public Product(string name, int price)
+        //    {
+        //        this.id = ++count;
+        //        this.name = name;
+        //        this.price = price;
+        //    }
+        //}
+        //static void Main(string[] args)
+        //{
+        //    Product product1 = new Product("Laptop", 50000);
+        //    Product product2 = new Product("Phone", 20000);
+        //    Product product3 = new Product("Tablet", 30000);
+        //    Console.WriteLine("Product ID: " + product1.id + "\tName: " + product1.name + "\tPrice: " + product1.price);
+        //    Console.WriteLine("Product ID: " + product2.id + "\tName: " + product2.name + "\tPrice: " + product2.price);
+        //    Console.WriteLine("Product ID: " + product3.id + "\tName: " + product3.name + "\tPrice: " + product3.price);
+        //}
+
+        //class Product
+        //{
+        //    public string name;
+        //    public int price;
+
+        //    public Product(string name, int price)
+        //    {
+        //        this.name = name;
+        //        this.price = price;
+        //    }
+        //    ~Product()
+        //    {
+        //        Console.WriteLine("Destructor called for product: " + name);
+        //    }
+        //}
+        //static void Main(string[] args)
+        //{
+        //    Product product1 = new Product("Laptop", 50000);
+        //}
+
+        //class Product
+        //{
+        //    private static int count;
+        //    public readonly int id;
+        //    public string name;
+        //    public int price;
+
+        //    public Product(string name, int price)
+        //    {
+        //        this.id = ++count;
+        //        this.name = name;
+        //        this.price = price;
+        //    }
+        //}
+        //static void Main(string[] args)
+        //{
+        //    Product product1 = new Product("Laptop", 50000);
+        //    Product product2 = new Product("Phone", 20000);
+        //    Product product3 = new Product("Tablet", 30000);
+        //    Console.WriteLine("Product ID: " + product1.id + "\tName: " + product1.name + "\tPrice: " + product1.price);
+        //    Console.WriteLine("Product ID: " + product2.id + "\tName: " + product2.name + "\tPrice: " + product2.price);
+        //    Console.WriteLine("Product ID: " + product3.id + "\tName: " + product3.name + "\tPrice: " + product3.price);
+        //}
+
+        //class Box
+        //{
+        //    private int width;
+        //    private int height;
+
+        //    public Box(int width, int height)
+        //    {
+        //        if (width > 0 && height > 0)
+        //        {
+        //            this.width = width;
+        //            this.height = height;
+        //        }
+        //        else
+        //        {
+        //            throw new ArgumentException("Width and Height must be positive integers.");
+        //        }
+        //    }
+
+        //    public int Area()
+        //    {
+        //        return this.width * this.height;
+        //    }
+
+        //    public int GetWidth()
+        //    {
+        //        return width;
+        //    }
+        //    public int GetHeight()
+        //    {
+        //        return height;
+        //    }
+
+        //    public void SetWidth(int width)
+        //    {
+        //        if (width > 0)
+        //            this.width = width;
+        //        else
+        //            throw new ArgumentException("Width must be a positive integer.");
+        //    }
+
+        //    public void SetHeight(int height)
+        //    {
+        //        if (height > 0)
+        //            this.height = height;
+        //        else
+        //            throw new ArgumentException("Height must be a positive integer.");
+        //    }
+        //}
+        //static void Main(string[] args)
+        //{
+        //    Box box = new Box(5, 10);
+        //    Console.WriteLine("Box Area: " + box.Area());
+        //    Console.WriteLine("Box Width: " + box.GetWidth());
+        //    Console.WriteLine("Box Height: " + box.GetHeight());
+        //    box.SetWidth(7);
+        //    box.SetHeight(3);
+        //    Console.WriteLine("Updated Box Area: " + box.Area());
+        //    Console.WriteLine("Updated Box Width: " + box.GetWidth());
+        //    Console.WriteLine("Updated Box Height: " + box.GetHeight());
+        //}
+
+        //class Box
+        //{
+        //    //private int width;
+        //    public int width
+        //    {
+        //        get { return width; }
+        //        set
+        //        {
+        //            if (value > 0)
+        //                width = value;
+        //            else
+        //                throw new ArgumentException("Width must be a positive integer.");
+        //        }
+        //    }
+        //    //private int height;
+        //    public int height
+        //    {
+        //        get { return height; }
+        //        set
+        //        {
+        //            if (value > 0)
+        //                height = value;
+        //            else
+        //                throw new ArgumentException("Height must be a positive integer.");
+        //        }
+        //    }
+        //    public Box(int width, int height)
+        //    {
+        //        width = width;
+        //        height = height;
+        //    }
+        //    public int Area()
+        //    {
+        //        return this.width * this.height;
+        //    }
+        //}
+        //static void Main(string[] args)
+        //{
+        //    Box box = new Box(5, -10);
+        //    box.width = 7;
+        //    box.height = 7;
+        //    Console.WriteLine("Box Area: " + box.Area());
+        //    Console.WriteLine("Box Width: " + box.width);
+        //    Console.WriteLine("Box Height: " + box.height);
+        //}
+
+    }
+}
